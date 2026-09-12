acaf3b2fcd3a5e88a88a425e0dc9e2f3ce6ff8f3
George <glukan07@bk.ru>
2026-09-12 14:08:52 +0700

    Добавлена вторая расчётная функция и пункт меню

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ double springForce(double k, double x) {
 	return k * x;
 }
 
+// Энергия упругости пружины
+double springEnergy(double k, double x) {
+	return k * x * x / 2;
+}
+
+
 // ===== Главная функция: меню =====
 int main() {
 	int choice;
@@ -17,6 +23,7 @@ int main() {
 	do {
 		cout << "\n=== Вариант 36: расчёты для пружины ===\n";
 		cout << "1. Сила упругости\n";
+		cout << "2. Энергия упругости\n";
 		cout << "0. Выход\n";
 		cout << "Выберите пункт: ";
 		cin >> choice;
@@ -25,6 +32,11 @@ int main() {
 			cout << "Введите значения k и x: ";
 			cin >> k >> x;
 			cout << "Сила упругости = " << springForce(k, x) << "\n";
+			break;
+		case 2:
+			cout << "Введите значения k и x: ";
+			cin >> k >> x;
+			cout << "Энергия упругости = " << springEnergy(k, x) << "\n";
 			break;
 		case 0:
 			cout << "Работа завершена.\n";
