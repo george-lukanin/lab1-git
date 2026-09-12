8d1767d61c30b9e539df318c06396d2f7a6c6fc5
George <glukan07@bk.ru>
2026-09-12 14:00:25 +0700

    Первый коммит: меню и функция варианта 36

---
+++ main.cpp
@@ -0,0 +1,37 @@
+// Лабораторная работа № 1. Вариант 36.
+// Выполнил: Луканин Г. В., группа ПИ-53.
+
+#include <iostream>
+using namespace std;
+
+// ===== Расчётные функции варианта =====
+// Сила упругости пружины
+double springForce(double k, double x) {
+	return k * x;
+}
+
+// ===== Главная функция: меню =====
+int main() {
+	int choice;
+	double k, x;
+	do {
+		cout << "\n=== Вариант 36: расчёты для пружины ===\n";
+		cout << "1. Сила упругости\n";
+		cout << "0. Выход\n";
+		cout << "Выберите пункт: ";
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			cout << "Введите значения k и x: ";
+			cin >> k >> x;
+			cout << "Сила упругости = " << springForce(k, x) << "\n";
+			break;
+		case 0:
+			cout << "Работа завершена.\n";
+			break;
+		default:
+			cout << "Такого пункта нет.\n";
+		}
+	} while (choice != 0);
+	return 0;
+}
